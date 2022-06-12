5aac82be2691fd8d279258030a51fb6d59363934
Vlad Korochanksiy <mginster234942@gmail.com>
2022-06-12 10:42:35 +0300

    sad

--- JS/index.js
+++ JS/index.js
@@ -369,21 +369,27 @@
 // };
 // getNumber(20, 1, 10);
 
-const SECRET_NUM = "20";
-let attempt = 0;
-const getUserSecretNum = function () {
-  for (let i = 1; i++; ) {
-    const user = prompt("Enter your num:");
-    if (user === SECRET_NUM) {
-      console.log("Lacky,you win!");
-      console.log("Количество попыток:" + attempt);
-      break;
-    } else if (user > SECRET_NUM) {
-      console.log("less");
-    } else if (user < SECRET_NUM) {
-      console.log("more");
-    }
-    attempt++;
-  }
+// const SECRET_NUM = "20";
+// let attempt = 0;
+// const getUserSecretNum = function () {
+//   for (let i = 1; i++; ) {
+//     const user = prompt("Enter your num:");
+//     if (user === SECRET_NUM) {
+//       console.log("Lacky,you win!");
+//       console.log("Количество попыток:" + attempt);
+//       break;
+//     } else if (user > SECRET_NUM) {
+//       console.log("less");
+//     } else if (user < SECRET_NUM) {
+//       console.log("more");
+//     }
+//     attempt++;
+//   }
+// };
+// getUserSecretNum();
+
+const obj1 = {
+  prop: 1,
 };
-getUserSecretNum();
+const obj2 = obj1;
+console.log((obj2.prop = "hi"));
